5f1888c62b30a180ef96c9724c9744ef36ceb699
dquangv <dquangvu7998@gmail.com>
2023-09-15 09:55:56 +0700

    Update lab 1 (connect to database)

--- labs/Labs/src/lab1/OutputDAO.java
+++ labs/Labs/src/lab1/OutputDAO.java
@@ -1,51 +1,68 @@
 package lab1;
 
+import java.sql.*;
 import java.util.Scanner;
 
 public class OutputDAO {
-    public static void main(String[] args) {
+
+    private static String url = "jdbc:sqlserver://localhost:1433;databaseName=Java2_Product;user=sa;password=123;encrypt=false;";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url);
+    }
+
+    public static void main(String[] args) throws SQLException {
         Scanner sc = new Scanner(System.in);
-        
+
         System.out.println("Amount of product:");
         int amountPro = Integer.parseInt(sc.nextLine());
-        
+
         Product[] products = new Product[amountPro];
-        
+
         for (int i = 0; i < products.length; i++) {
             System.out.println("Product " + (i + 1));
-            
+
             System.out.println("Name: ");
             String name = sc.nextLine();
-            
+
             System.out.println("Price: ");
             double price = Double.parseDouble(sc.nextLine());
-            
+
             System.out.println("Is it tax-exempt? (yes/no)");
             String isTaxExempt = sc.nextLine();
-            
+
             if (isTaxExempt.equalsIgnoreCase("yes")) {
                 products[i] = new NoTaxProduct();
             } else {
                 products[i] = new Product();
             }
-            
+
             products[i].setName(name);
             products[i].setPrice(price);
         }
-        
+
         System.out.println("");
         System.out.println("Product information");
-        
+
         for (int i = 0; i < products.length; i++) {
-            System.out.println("Product " + (i+1));
+            System.out.println("Product " + (i + 1));
             System.out.println("Name: " + products[i].getName());
             System.out.println("Price: " + products[i].getPrice());
             System.out.println("Tax: " + products[i].getImportTax());
             products[i].insert();
-            products[i].update();
-            products[i].delete();
-            products[i].select();
+//            products[i].update();
+//            products[i].delete();
+//            products[i].select();
             System.out.println("");
+        }
+
+        Connection con = getConnection();
+        Statement stm = con.createStatement();
+        ResultSet rs = stm.executeQuery("select * from product");
+
+        while (rs.next()) {
+            System.out.println(rs.getString("name"));
+            System.out.println(rs.getString("price"));
         }
     }
 }
--- labs/Labs/src/lab1/Product.java
+++ labs/Labs/src/lab1/Product.java
@@ -1,4 +1,8 @@
 package lab1;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Product implements DAO {
 
@@ -12,7 +16,7 @@ public class Product implements DAO {
 
     public Product() {
     }
-    
+
     public double getImportTax() {
         return this.price * 0.1;
     }
@@ -35,21 +39,62 @@ public class Product implements DAO {
 
     @Override
     public void insert() {
-        System.out.println("Insert successfully!");
+        try (Connection conn = OutputDAO.getConnection()) {
+            String sql = "insert into product (name, price) values (?, ?)";
+            try (PreparedStatement statement = conn.prepareStatement(sql)) {
+                statement.setString(1, this.name);
+                statement.setDouble(2, this.price);
+                statement.executeUpdate();
+                System.out.println("Insert successfully!");
+            }
+        } catch (SQLException e) {
+        }
     }
 
     @Override
     public void update() {
-        System.out.println("Update successfully!");
+        try (Connection conn = OutputDAO.getConnection()) {
+            String sql = "update product set price = ? where name = ?";
+            try (PreparedStatement statement = conn.prepareStatement(sql)) {
+                statement.setDouble(1, this.price);
+                statement.setString(2, this.name);
+                statement.executeUpdate();
+                System.out.println("Update successfully!");
+            }
+        } catch (SQLException e) {
+        }
     }
 
     @Override
     public void delete() {
-        System.out.println("Delete successfully!");
+        try (Connection conn = OutputDAO.getConnection()) {
+            String sql = "delete from product where name = ?";
+            try (PreparedStatement statement = conn.prepareStatement(sql)) {
+                statement.setString(1, this.name);
+                statement.executeUpdate();
+                System.out.println("Delete successfully!");
+            }
+        } catch (SQLException e) {
+        }
     }
 
     @Override
     public void select() {
-        System.out.println("Select successfully!");
+        try (Connection conn = OutputDAO.getConnection()) {
+            String sql = "select name, price from product where name = ?";
+            try (PreparedStatement statement = conn.prepareStatement(sql)) {
+                statement.setString(1, this.name);
+                ResultSet rs = statement.executeQuery();
+                while (rs.next()) {
+                    String productName = rs.getString("Name");
+                    double productPrice = rs.getDouble("Price");
+                    System.out.println("Name: " + productName);
+                    System.out.println("Price: " + productPrice);
+                }
+                System.out.println("Select successfully!");
+            }
+        } catch (SQLException e) {
+        }
     }
+
 }
